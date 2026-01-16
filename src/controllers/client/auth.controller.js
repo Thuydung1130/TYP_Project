b36fbfd57ec27f35372a57184dcaacdea792c788
ThuyDung <phungthuydung1103@gmail.com>
2026-01-16 15:02:41 +0700

    update-runner-submit

--- src/controllers/client/auth.controller.js
+++ src/controllers/client/auth.controller.js
@@ -36,7 +36,7 @@ export const googleLogin = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, email: user.email },
       process.env.JWT_SECRET,
-      { expiresIn: '7d' }
+      { expiresIn: '1d' }
     );
     console.log(user);
     res.json({
--- src/controllers/client/problem.controller.js
+++ src/controllers/client/problem.controller.js
@@ -1,6 +1,8 @@
 import Problem from "../../model/problem.model.js";
 import Testcase from "../../model/testcase.model.js";
-import { saveCodeToFile, compileCpp, runCpp } from "../../../sandbox/runner.js";
+import  fs from "fs";
+import path from "path";
+import { createSubmissionDir,saveCodeToFile, compileCpp, runCpp,cleanupSubmission } from "../../../sandbox/runner.js";
 // GET /problem
 export const index = async (req, res) => {
   try {
@@ -28,8 +30,11 @@ export const getProblem = async (req, res) => {
 
 
 export const submitCode = async (req, res) => {
+//   console.log("req.file:", req.file);
+// console.log("req.body:", req.body);
+  let submissionDir = null; // ✅ ADD
   try {
-
+    
     const { problemId, code } = req.body;
     let cppPath = "";
     //console.log(problemId)
@@ -39,18 +44,27 @@ export const submitCode = async (req, res) => {
     const timeLimit = problem.time_limit || 1000; // ms
     const memoryLimit = problem.memory_limit || 256; // MB
 
+    // ===============================
+    // ✅ ADD: tạo folder riêng cho submission
+    // ===============================
+    submissionDir = createSubmissionDir();
 
+    //  if (code) {
+    //   saveCodeToFile(code, submissionDir);
+    // }
     if (req.file) {
-      cppPath = req.file.path;
+      const filePath = path.join(submissionDir, "Main.cpp");
+      fs.writeFileSync(filePath, req.file.buffer);
     } else if (code) {
       // 1. Save file
-      await saveCodeToFile(code);
+      await saveCodeToFile(code,submissionDir);
     }
-
-
+    // 🔥 DEBUG
+//console.log("Submission dir:", submissionDir);
+//console.log("Files:", fs.readdirSync(submissionDir));
 
     // 2. Compile
-    const compileResult = await compileCpp();
+    const compileResult = await compileCpp(submissionDir);
 
     // 3. Nếu compile lỗi → trả luôn kết quả
     if (!compileResult.success) {
@@ -59,22 +73,27 @@ export const submitCode = async (req, res) => {
         error: compileResult.error
       });
     }
-    //const testcases1 = await Testcase.find();
-    //console.log(testcases1);
+    
     // 3. Lấy testcases của problem
     const testcases = await Testcase.find({ problem_id: problemId });
     //console.log(testcases);
     let results = [];
-    let passed = 0;
+    
     let finalStatus = "AC"; // mặc định
 
     // 4. Chạy từng test case
     for (let tc of testcases) {
-      const r = await runCpp(tc.input, timeLimit, memoryLimit);
+      // ✅ FIX: truyền submissionDir
+      const r = await runCpp(
+        submissionDir,
+        tc.input,
+        timeLimit,
+        memoryLimit
+      );
 
       let actual = (r.stdout || "").trim();
       const expected = tc.output.trim();
-      let isPass = false;
+      //let isPass = false;
       let caseStatus = "AC";
 
 
@@ -91,22 +110,11 @@ export const submitCode = async (req, res) => {
         caseStatus = "RE";
         finalStatus = "RE";
       }
-      else if (r.status === "OK") {
-        if (actual !== expected) {
-          caseStatus = "WA";
-          if (finalStatus === "AC") finalStatus = "WA";
-        }
+      else if (r.status === "OK" && actual !== expected) {
+        caseStatus = "WA";
+        if (finalStatus === "AC") finalStatus = "WA";
       }
-      // if (r.status === "OK") {
-      //     actual = r.stdout.trim();
-      //     isPass = actual === expected;
-      // } else {
-      //     actual = r.stderr;
-      //     isPass = false;
-      // }
-
-
-      //if (isPass) passed++;
+     
 
 
       results.push({
@@ -124,22 +132,127 @@ export const submitCode = async (req, res) => {
       // Nếu gặp lỗi nặng → dừng sớm
       if (["TLE", "MLE", "RE"].includes(caseStatus)) break;
     }
-    console.log(results);
+    //console.log(results);
     // 5. Trả kết quả cuối
     return res.json({
       status: finalStatus,
       results
-      // status: "done",
-      // total: testcases.length,
-      // passed,
-      // time_limit: timeLimit,
-      // memory_limit: memoryLimit,
-      // results
+      
     });
 
 
   } catch (error) {
     return res.status(500).json({ error: error.message });
+  }finally {
+    // ===============================
+    // ✅ ADD: cleanup folder (RẤT QUAN TRỌNG)
+    // ===============================
+    if (submissionDir) {
+      cleanupSubmission(submissionDir);
+    }
   }
 
 };
+
+// export const submitCode = async (req, res) => {
+//   try {
+
+//     const { problemId, code } = req.body;
+//     let cppPath = "";
+//     //console.log(problemId)
+
+//     // Lấy thông tin đề bài (time limit + memory limit)
+//     const problem = await Problem.findById(problemId);
+//     const timeLimit = problem.time_limit || 1000; // ms
+//     const memoryLimit = problem.memory_limit || 256; // MB
+
+
+//     if (req.file) {
+//       cppPath = req.file.path;
+//     } else if (code) {
+//       // 1. Save file
+//       await saveCodeToFile(code);
+//     }
+
+
+
+//     // 2. Compile
+//     const compileResult = await compileCpp();
+
+//     // 3. Nếu compile lỗi → trả luôn kết quả
+//     if (!compileResult.success) {
+//       return res.json({
+//         status: "CE",
+//         error: compileResult.error
+//       });
+//     }
+//     //const testcases1 = await Testcase.find();
+//     //console.log(testcases1);
+//     // 3. Lấy testcases của problem
+//     const testcases = await Testcase.find({ problem_id: problemId });
+//     //console.log(testcases);
+//     let results = [];
+//     let passed = 0;
+//     let finalStatus = "AC"; // mặc định
+
+//     // 4. Chạy từng test case
+//     for (let tc of testcases) {
+//       const r = await runCpp(tc.input, timeLimit, memoryLimit);
+
+//       let actual = (r.stdout || "").trim();
+//       const expected = tc.output.trim();
+//       let isPass = false;
+//       let caseStatus = "AC";
+
+
+//       // -- Check lỗi theo thứ tự ưu tiên --
+//       if (r.status === "TLE") {
+//         caseStatus = "TLE";
+//         finalStatus = "TLE";
+//       }
+//       else if (r.status === "MLE") {
+//         caseStatus = "MLE";
+//         finalStatus = "MLE";
+//       }
+//       else if (r.status === "RTE") {
+//         caseStatus = "RE";
+//         finalStatus = "RE";
+//       }
+//       else if (r.status === "OK") {
+//         if (actual !== expected) {
+//           caseStatus = "WA";
+//           if (finalStatus === "AC") finalStatus = "WA";
+//         }
+//       }
+     
+
+
+//       results.push({
+
+//         input: tc.input,
+//         expected,
+//         actual,
+//         //pass: isPass,
+//         status: caseStatus,
+//         time: r.time,
+//         memory: r.memory
+//       });
+
+
+//       // Nếu gặp lỗi nặng → dừng sớm
+//       if (["TLE", "MLE", "RE"].includes(caseStatus)) break;
+//     }
+//     //console.log(results);
+//     // 5. Trả kết quả cuối
+//     return res.json({
+//       status: finalStatus,
+//       results
+      
+//     });
+
+
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+
+// };
